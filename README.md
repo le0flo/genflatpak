eb307482a43af2928c2444d7951a41ec58f4a81b
Leonardo <leonflorenzano@gmail.com>
2023-11-29 12:06:58 +0000

    Repository creata

---
+++ README.md
@@ -0,0 +1,18 @@
+# DMENU flatpak link generator
+
+This bash script generates executable links for every flatpak app and puts it in /usr/bin
+This comes with a deleter also, that deletes every file previously generated, to keep things clean.
+
+# Generating
+
+Execute the script without arguments
+
+# Deleting
+
+Just put anythings you want as an argument, that will trigger the delete function
+
+> Made during school hours swag
+
+### Warning
+
+Make sure that you chmod +x the file before trying to run it
---
+++ generate.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+appIDs=()
+
+while IFS= read -r line; do
+    appIDs+=( "$line" )
+done < <( flatpak list --app --columns=application | grep -v "Application ID" )
+
+appNames=()
+
+while IFS= read -r line; do
+    appNames+=( "$line" )
+done < <( flatpak list --app --columns=name | grep -v "Name" )
+
+appNum=${#appIDs[@]}
+
+dir="$( getent passwd "$USER" | cut -d: -f6 )/.config/flatpakApps"
+
+[ -d $dir ] || mkdir -p $dir
+
+
+
+if [ $# -eq 0  ];
+then
+	for (( i=0; i<=$appNum-1; i++ ))
+	do
+    		file="$dir/${appIDs[$i]}.sh"
+    		touch $file
+    		echo "" > $file
+    		echo '#!/bin/bash' >> $file
+    		echo "flatpak run ${appIDs[$i]}" >> $file
+
+    		chmod +x $file
+
+    		linkname=( ${appNames[$i]} )
+    		link="/usr/bin/${linkname[0]}"
+		
+		echo ${link,,}
+    		sudo ln $file ${link,,}
+	done
+
+	echo "Links created successfully"
+else
+	for (( i=0; i<=$appNum-1; i++ ))
+	do
+		linkname=( ${appNames[$i]} )
+		link="/usr/bin/${linkname[0]}"
+		
+		echo ${link,,}
+		sudo rm -rf ${link,,}
+	done
+
+	echo "Links deleted successfully"
+fi
